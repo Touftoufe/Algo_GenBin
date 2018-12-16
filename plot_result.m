8f74041373fb97023984fe80da44eeb52d88a7c0
Touftoufe <touftoufeokami@gmail.com>
2018-12-16 04:19:00 +0100

    Update plot_result.m

--- plot_result.m
+++ plot_result.m
@@ -3,6 +3,15 @@ close all
 clc
 
 load('matlab.mat');
+
+subplot(1,2,1)
 plot(lambda,J,lambda,f(lambda,c(1), c(2), c(3), c(4)),'r');
-figure(2)
+xlabel("Lambda")
+ylabel("J")
+title("Approximation of the experimental data")
+
+subplot(1,2,2)
 plot(lambda,JC,lambda,f(lambda,c(1), c(2), c(3), c(4)),'r');
+xlabel("Lambda")
+ylabel("J")
+title("Comparition with the approximation data provided in the 'profile.txt' file")
